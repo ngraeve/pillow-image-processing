4efc558e95fa35d0f63854f3669bc56431f09bdd
Nick Graeve <gravy.nick@gmail.com>
2025-08-07 19:33:18 -0500

    fixed import

--- infra/app.py
+++ infra/app.py
@@ -3,7 +3,7 @@
 Entry point for AWS CDK application.
 """
 import aws_cdk as cdk
-from cdk.stack import PillowImageProcessingStack
+from infra.stack import PillowImageProcessingStack
 
 app = cdk.App()
 PillowImageProcessingStack(app, "PillowImageProcessingStack")
